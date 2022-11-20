9e824f38845d04627c6e949b5084761fd21f3f37
Lariva23 <aviraljainsfc@gmail.com>
2022-11-20 14:50:13 +0530

    updates

--- utils/layers/DenseLayer.cpp
+++ utils/layers/DenseLayer.cpp
@@ -5,35 +5,28 @@
 #include <cstdlib>
 #include <iostream>
 #include <time.h>
-#include "linalg.h"
 #include "DenseLayer.h"
-
 #include "linalg.h"
 
+namespace ANN{
 
-int input_features;
-        int output_features;
-        std::vector<std::vector<double>> weights, z, a_prev;
-        std::vector<double> bias; 
-        std::string activation;
-
-
-DenseLayer::DenseLayer(int input_features, int output_features, std::string activation = "linear",
- std::string weight_initialiser = "random", 
- std::string bias_initialiser = "zero", std::pair<double,double> grad_clip = std::make_pair<double,double>(-100.0, 100.0))
-{
-    input_features=25;
-    output_features=10;
-    // grad_clip=(-100,100);
-    initialise(weight_initialiser,bias_initialiser);
- 
-}
-void initialise(std::string weight_initialiser = "random", std::string bias_initialiser = "zero")
-{
-    srand(time(0));
-    for(int i=0;i<weights.size();i++)
+    DenseLayer::DenseLayer(int input_features, int output_features, std::string activation = "linear",
+    std::string weight_initialiser = "random", 
+    std::string bias_initialiser = "zero", std::pair<double,double> grad_clip = std::make_pair<double,double>(-100.0, 100.0))
     {
-        for(int j=0;j<weights[0].size();j++)
+        input_features=25;
+        output_features=10;
+        // grad_clip=(-100,100);
+        weights = std::vector(input_features, std::vector<double>(output_features, 0.0));
+        bias = std::vector(output_features, std::vector<double>(1, 0.0));
+        
+        initialise(weight_initialiser,bias_initialiser);
+    
+    }
+    void DenseLayer::initialise(std::string weight_initialiser = "random", std::string bias_initialiser = "zero")
+    {
+        srand(time(0));
+        for(int i=0;i<weights.size();i++)
         {
             for(int j=0;j<weights[0].size();j++)
             {
@@ -49,24 +42,18 @@ void initialise(std::string weight_initialiser = "random", std::string bias_init
         
     }
 
-std::vector<std::vector<double>> feedforward(std::vector<std::vector<double>> a_prev)
-{
-    std::vector<std::vector<double>> z;
-    std::vector<std::vector<double>> a;
-    int r=a_prev.size();
-    int c=a_prev[0].size();
-    for(int i=0;i<r;i++)
+    std::vector<std::vector<double>> DenseLayer::feedforward(std::vector<std::vector<double>> a_prev)
     {
-        for(int j=0;j<c;j++)
-        {
-            ANN::multiply(z,a_prev,weights);
-        }
-    }
+        std::vector<std::vector<double>> z;
+        std::vector<std::vector<double>> a;
+        int r=a_prev.size();
+        int c=a_prev[0].size();
 
-    ANN::add(z,z,bias);
+        ANN::multiply(z,a_prev,weights);
 
-    
-    ANN::sigmoid(a,z);
+        ANN::add(z,z,bias);
+        
+        ANN::sigmoid(a,z);
 
         
         return a;
